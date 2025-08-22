733be7757e99813e2ab7df9fd519e877754050d6
pauloborini <86570695+pauloborini@users.noreply.github.com>
2025-08-22 18:47:57 -0300

    pequenos ajustes de versao de package e import

--- lib/features/home/view/components/city_dropdown_button.dart
+++ lib/features/home/view/components/city_dropdown_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:motels/core/helpers/extensions/responsive_extension.dart';
 import 'package:motels/core/helpers/ui/modal_helper.dart';
 import 'package:motels/core/ui/styles/colors_app.dart';
